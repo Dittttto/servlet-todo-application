96f3e487df27acb2f5fc0eecbe2240bd2302a9c8
DevFrog92 <yongsu1481@gmail.com>
2023-12-08 20:47:17 +0900

    #6 implement TodoDTO class

---
+++ src/main/java/org/todo/dto/TodoDTO.java
@@ -0,0 +1,28 @@
+package org.todo.dto;
+
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.todo.model.Todo;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoDTO {
+        private Long id;
+        private String content;
+        private boolean done;
+        private LocalDate dueDate;
+
+        public static TodoDTO fromModel(Todo todo) {
+                return TodoDTO.builder()
+                        .id(todo.getId())
+                        .content(todo.getContent())
+                        .dueDate(todo.getDueDate())
+                        .done(todo.isDone())
+                        .build();
+        }
+}
