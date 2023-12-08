a8fced8d692ccd2515f3fc290a12259abff52e30
DevFrog92 <yongsu1481@gmail.com>
2023-12-08 20:47:36 +0900

    #6 implement TodoService enum class

---
+++ src/main/java/org/todo/service/TodoService.java
@@ -0,0 +1,67 @@
+package org.todo.service;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.extern.log4j.Log4j2;
+import org.todo.model.Todo;
+import org.todo.dto.TodoDTO;
+import org.todo.repository.TodoRepository;
+
+@Log4j2
+public enum TodoService {
+        INSTANCE;
+
+        private final TodoRepository repository = TodoRepository.INSTANCE;
+
+        public List<TodoDTO> getList() {
+                try {
+                        return repository.getList().stream().map(TodoDTO::fromModel).collect(Collectors.toList());
+                } catch (SQLException e) {
+                        log.error(e.getMessage());
+                        throw new RuntimeException(e);
+                }
+        }
+
+        public void insert(TodoDTO dto) {
+                try {
+                        repository.insert(
+                                Todo.builder()
+                                        .content(dto.getContent())
+                                        .dueDate(LocalDate.now())
+                                        .done(false)
+                                        .build()
+                        );
+                } catch (SQLException e) {
+                        log.error(e);
+                        throw new RuntimeException(e);
+                }
+        }
+
+        public void update(TodoDTO updateTodo) {
+                try {
+                        Todo origin = repository.getById(updateTodo.getId());
+                        repository.update(Todo.builder()
+                                .id(origin.getId())
+                                .content(updateTodo.getContent())
+                                .done(updateTodo.isDone())
+                                .dueDate(origin.getDueDate())
+                                .build());
+                } catch (SQLException e) {
+                        log.error(e.getMessage());
+                        throw new RuntimeException(e);
+                }
+
+        }
+
+        public void delete(TodoDTO updateTodo) {
+                try {
+                        repository.deleteById(updateTodo.getId());
+                } catch (SQLException e) {
+                        log.error(e.getMessage());
+                        throw new RuntimeException(e);
+                }
+
+        }
+}
