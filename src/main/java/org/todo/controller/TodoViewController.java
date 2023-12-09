cfb55cee454b63545f66410bf623c655536dde09
DevFrog92 <yongsu1481@gmail.com>
2023-12-09 19:01:44 +0900

    #13 refactoring TodoApiController and TodoViewController for memberId parameter

--- src/main/java/org/todo/controller/TodoViewController.java
+++ src/main/java/org/todo/controller/TodoViewController.java
@@ -8,19 +8,23 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import lombok.extern.log4j.Log4j2;
+import org.todo.dto.MemberDTO;
 import org.todo.dto.TodoDTO;
 import org.todo.service.TodoService;
 
 @Log4j2
-@WebServlet(name = "todoViewController", urlPatterns = "/")
+@WebServlet(name = "todoViewController", urlPatterns = "/todo")
 public class TodoViewController extends HttpServlet {
         private final TodoService service = TodoService.INSTANCE;
 
         @Override
         protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
                 log.info("==== view controller ==== ");
-                List<TodoDTO> list = service.getList();
+                HttpSession session = req.getSession();
+                MemberDTO attribute = (MemberDTO)session.getAttribute("loginInfo");
+                List<TodoDTO> list = service.getList(attribute.getId());
                 req.setAttribute("todos", list);
                 req.getRequestDispatcher("/WEB-INF/todo/home.jsp").forward(req, resp);
         }
